796e76aba92b3a44716b87a0662f7cd0a965b63d
Nisarg Bhavsar <86922099+NisargBhavsar25@users.noreply.github.com>
2023-12-13 08:07:22 +0530

    added argument for inpur path

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import cv2
 import numpy as np
 from PIL import Image
 from kmeans import KMeans
+import argparse
 
-image = cv2.imread('images\input-image.jpg')
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Image Segmentation using k-means clustering.")
+parser.add_argument('--path', type=str, default='images/input-image.jpg', help='Path to the input image')
+args = parser.parse_args()
+
+# Read the image from the provided path
+image = cv2.imread(args.path)
 
 raw = np.float32(image.reshape((-1, 3)))
 
@@ -12,25 +19,25 @@ ks = [1, 2, 3, 4, 5, 6, 8, 10, 14, 16, 20, 25, 40, 50]
 for k in ks:
     model = KMeans(k=k)
     model.fit(raw)
-    segemented_raw = np.zeros(raw.shape)
+    segmented_raw = np.zeros(raw.shape)
 
     for i, pixel in enumerate(raw):
-        segemented_raw[i] = np.int64(model.predict(pixel))
+        segmented_raw[i] = np.int64(model.predict(pixel))
 
-    segemented = segemented_raw.reshape(image.shape)
-    segemented = cv2.putText(
-        segemented, f"k={k}", (330, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 2)
-    cv2.imwrite(f"images\segmented-images\k_{k}.jpg", segemented)
+    segmented = segmented_raw.reshape(image.shape)
+    segmented = cv2.putText(
+        segmented, f"k={k}", (330, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 2)
+    cv2.imwrite(f"images/segmented-images/k_{k}.jpg", segmented)
     print(f"k_{k}.jpg outputed")
 
-frames = [Image.open(f"images\segmented-images\k_{i}.jpg")
+frames = [Image.open(f"images/segmented-images/k_{i}.jpg")
           for i in ks]
 frame_one = frames[0]
-frame_one.save("images\output.gif", format="GIF", append_images=frames,
+frame_one.save("images/output.gif", format="GIF", append_images=frames,
                save_all=True, duration=750, loop=0)
 
 print('''
 -------------------------
         Done!!
 -------------------------
-        ''')
+''')
